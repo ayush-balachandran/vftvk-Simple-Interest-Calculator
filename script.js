9972afe7c26252f254157c8a9abfd41706b6b668
ayush-balachandran <ayush.balachandran@gmail.com>
2021-05-12 16:43:53 +0530

    updated project code

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+//function to display rate value from slider
+function update(val)
+{
+    document.getElementById("rate_val").innerHTML = val;
+}
+
+//function to compute the intrest and total amount
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate =  document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    //validation of principal to check for negative or zero value
+    if(principal && parseInt(principal)>0)
+    {
+        var intrest = (principal * rate * years)/100;
+        var final = parseInt(principal) + parseInt(intrest);
+        var year = parseInt(new Date().getFullYear())+parseInt(years);
+        document.getElementById("result").innerHTML= "If you deposit "+principal+",\<br\> at an interest rate of "+rate+"%\<br\>You will receive an amount of "+final+",\<br\>in the year "+year;
+    }
+    else
+    {
+        document.getElementById("result").innerHTML= "Enter a positive number";
+        document.getElementById("principal").focus();
+    }
 }
+
         
